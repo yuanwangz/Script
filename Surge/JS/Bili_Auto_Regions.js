058a86f0e27af2c37c9148ca32f0163a03d59557
yuanwangz <34086226+yuanwangz@users.noreply.github.com>
2022-06-19 18:42:34 +0800

    Update Bili_Auto_Regions.js

--- Surge/JS/Bili_Auto_Regions.js
+++ Surge/JS/Bili_Auto_Regions.js
@@ -77,10 +77,10 @@ let run = EnvInfo();
 
 async function SwitchRegion(play) {
 	const Group = $.read('BiliArea_Policy') || '港台番剧'; //Your blibli policy group name.
-	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
+	const CN = $.read('BiliArea_CN') || 'direct'; //Your China sub-policy name.
 	const TW = $.read('BiliArea_TW') || '台湾节点'; //Your Taiwan sub-policy name.
 	const HK = $.read('BiliArea_HK') || '香港节点'; //Your HongKong sub-policy name.
-	const DF = $.read('BiliArea_DF') || 'DIRECT'; //Sub-policy name used after region is blocked(e.g. url 404)
+	const DF = $.read('BiliArea_DF') || 'direct'; //Sub-policy name used after region is blocked(e.g. url 404)
 	const off = $.read('BiliArea_disabled') || ''; //WiFi blacklist(disable region change), separated by commas.
 	const current = await $.getPolicy(Group);
 	const area = (() => {
